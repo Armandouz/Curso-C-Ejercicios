c8e1b8783f89c8a9cd58751f3bbaff2a3b4a61bd
Armandouz <163364602+Armandouz@users.noreply.github.com>
2025-01-19 23:12:23 -0600

    Add files via upload

---
+++ 02. tipos de datos basicos/2. tipos de datos basicos/2. tipos de datos basicos.cpp
@@ -0,0 +1,23 @@
+// 2. tipos de datos basicos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int entero = 15;
+    float flotante = 10.45;
+    double mayor = 16.3456;
+    char letra = 'a';
+
+
+
+
+
+    cout << letra;
+    cout << flotante;
+
+    return 0;
+}
+
